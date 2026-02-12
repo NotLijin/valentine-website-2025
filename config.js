a41952abcbb37d288f67deed538adeff032e99ce
NotLijin <leahli040430@gmail.com>
2026-02-12 02:18:42 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "MR.🐹",
+    valentineName: "Martin🐹",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
